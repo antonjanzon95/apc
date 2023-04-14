8372bae5b17eb2c6fcaf3cfb252ee98e18ec81c7
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-04-14 17:37:01 +0200

    nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../lib/mongodb";
 import EmailProvider from "next-auth/providers/email";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
@@ -10,6 +11,10 @@ export default NextAuth({
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID ?? "",
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? "",
     }),
   ],
 });
